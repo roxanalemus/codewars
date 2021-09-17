fb80b9e4f32386b42a6f872693ec2d3b95a7dab0
roxanalemus <roxanalemusdev@gmail.com>
2021-09-16 22:52:26 -0400

    adding one codewar

---
+++ grasshopperMessiGoals.js
@@ -0,0 +1,5 @@
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+  }
+
+  //https://www.codewars.com/kata/55f73be6e12baaa5900000d4
