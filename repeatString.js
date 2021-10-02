fada53af3aefc91259672bc809a3adf9c4ff02a9
roxanalemus <roxanalemusdev@gmail.com>
2021-10-02 19:01:07 -0400

    adding two new codewars

---
+++ repeatString.js
@@ -0,0 +1,5 @@
+function repeatStr (n, s) {
+    return s.repeat(n);
+ }
+
+ //https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
---
+++ reversedArrayOfDigits.js
@@ -0,0 +1,16 @@
+function digitize(n) {
+    //get non-negative number
+    //return in string
+    //reverse string
+    //convert each 
+    //n.split("")
+    const arr = (n.toString().split('').reverse())
+    return(arr).map(element => Number(element))
+    //console.log(arr)
+    
+   //arr.forEach(element => (element));
+    console.log(arr)
+  
+  }
+
+  //https://www.codewars.com/kata/5583090cbe83f4fd8c000051
