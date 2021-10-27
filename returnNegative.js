3bfb5ef123429f2ab0d15cc0849e764438baaaed
roxanalemus <roxanalemusdev@gmail.com>
2021-10-27 17:43:02 -0400

    adding one new codewar

--- returnNegative.js
+++ returnNegative.js
@@ -1,9 +1,5 @@
 function makeNegative(num) {
-    if(num < 0){
-        return(num)
-    }else{
-        return (-num)
-    }
-  }
+    return num < 0 ? num : num * -1
+}
 
   //https://www.codewars.com/kata/55685cd7ad70877c23000102
---
+++ twiceAsOld.js
@@ -0,0 +1,5 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return dadYearsOld > 2 * sonYearsOld ? dadYearsOld - (2 * sonYearsOld) : (2 * sonYearsOld) - dadYearsOld 
+   }
+
+   //https://www.codewars.com/kata/5b853229cfde412a470000d0
