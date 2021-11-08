836b7b4444f5b873bbb29c18306c22895e1fc423
roxanalemus <roxanalemusdev@gmail.com>
2021-11-08 17:22:42 -0500

    adding one new file

---
+++ keepUpTheHoop.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/solutions/javascript
+
+function hoopCount (n) {
+    return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"  
+ }
