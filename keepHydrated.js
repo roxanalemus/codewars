43de532051b90df2179e002ab18f1c02e4eb5ba5
roxanalemus <roxanalemusdev@gmail.com>
2021-11-09 17:24:53 -0500

    adding one new file

---
+++ keepHydrated.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/582cb0224e56e068d800003c
+
+function litres(time) {
+    //multiply time nathan has cycled by .5 liters
+    //round that down to closest integer
+     return Math.floor(time * .5)
+  
+  }
