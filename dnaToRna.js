eb546d66bccbde9ee138fe32a6e616045c34eca9
roxanalemus <roxanalemusdev@gmail.com>
2021-11-26 17:53:47 -0500

    adding one new file

---
+++ dnaToRna.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/5556282156230d0e5e000089
+
+function DNAtoRNA(dna) {
+    return dna.replace(/T/g, "U")// create a function which returns an RNA sequence from the given DNA sequence
+  }
