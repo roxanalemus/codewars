717f25e5643ce0c6c729df595549e59491a5b5ff
roxanalemus <roxanalemusdev@gmail.com>
2021-12-13 10:37:21 -0500

    adding one new file

---
+++ drinkAbout.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/56170e844da7c6f647000063
+
+function peopleWithAgeDrink(old) {
+    if(old < 14){
+      return "drink toddy"
+    }if(old < 18){
+      return "drink coke"
+    }if(old < 21){
+      return "drink beer"
+    }if(old >= 21)
+    return "drink whisky"
+  };
